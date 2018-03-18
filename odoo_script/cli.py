677766635194ee9143a5595a64952175b9a3118f
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-03-18 23:16:02 +0100

    Refactor options.
    Environment-related options are now in a reusable decorator
    named odoo_env_options, making it very easy to create new scripts
    with specific options.

--- odoo_script/cli.py
+++ odoo_script/cli.py
@@ -1,6 +1,7 @@
 # Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+import functools
 import logging
 import runpy
 import sys
@@ -13,24 +14,38 @@ from .env import OdooEnvironment
 _logger = logging.getLogger(__name__)
 
 
+def odoo_env_options(func):
+    @click.option('--config', '-c', envvar=['ODOO_RC', 'OPENERP_SERVER'],
+                  type=click.Path(exists=True, dir_okay=False),
+                  help="Specify the Odoo configuration file. Other "
+                       "ways to provide it are with the ODOO_RC or "
+                       "OPENERP_SERVER environment variables, or ~/.odoorc "
+                       "(Odoo >= 10) or ~/.openerp_serverrc.")
+    @click.option('--database', '-d', envvar=['PGDATABASE'],
+                  help="Specify the database name.")
+    @click.option('--log-level',
+                  help="Specify the logging level. Accepted values depend "
+                       "on the Odoo version, and include debug, info "
+                       "(default), warn, error.")
+    @functools.wraps(func)
+    def wrapper(config, database, log_level, *args, **kwargs):
+        with OdooEnvironment(
+            config=config,
+            database=database,
+            log_level=log_level,
+        ) as env:
+            return func(env, *args, **kwargs)
+    return wrapper
+
+
 @click.command(help="Execute a python script in an initialized Odoo "
                     "environment. The script has access to a 'env' global "
-                    "variable which is the odoo.api.Environment initialized "
-                    "for the given database. If no script is provided, "
-                    "the script is read from stdin or an interactive console "
-                    "is started if stdin appears to be a terminal.")
-@click.option('--config', '-c', envvar=['ODOO_RC', 'OPENERP_SERVER'],
-              type=click.Path(exists=True, dir_okay=False),
-              help="Specify the Odoo configuration file. Other "
-                   "ways to provide it are with the ODOO_RC or "
-                   "OPENERP_SERVER environment variables, or ~/.odoorc "
-                   "(Odoo >= 10) or ~/.openerp_serverrc.")
-@click.option('--database', '-d', envvar=['PGDATABASE'],
-              help="Specify the database name.")
-@click.option('--log-level',
-              help="Specify the logging level. Accepted values depend on the "
-                   "Odoo version, and include debug, info (default), warn, "
-                   "error.")
+                    "variable which is the odoo.api.Environment "
+                    "initialized for the given database. If no script is "
+                    "provided, the script is read from stdin or an "
+                    "interactive console is started if stdin appears "
+                    "to be a terminal.")
+@odoo_env_options
 @click.option('--interactive/--no-interactive', '-i',
               help="Inspect interactively after running the script.")
 @click.option('--shell-interface',
@@ -40,16 +55,10 @@ _logger = logging.getLogger(__name__)
 @click.argument('script', required=False,
                 type=click.Path(exists=True, dir_okay=False))
 @click.argument('script-args', nargs=-1)
-def main(config, database, log_level, interactive, shell_interface,
-         script, script_args):
-    with OdooEnvironment(
-        config=config,
-        database=database,
-        log_level=log_level,
-    ) as env:
-        global_vars = {'env': env}
-        if script:
-            sys.argv[1:] = script_args
-            global_vars = runpy.run_path(script, init_globals=global_vars)
-        if not script or interactive:
-            interact(global_vars, shell_interface, interactive)
+def main(env, interactive, shell_interface, script, script_args):
+    global_vars = {'env': env}
+    if script:
+        sys.argv[1:] = script_args
+        global_vars = runpy.run_path(script, init_globals=global_vars)
+    if not script or interactive:
+        interact(global_vars, shell_interface, interactive)
