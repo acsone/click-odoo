cbf1778062a0ed92fd2982401bf3aa541fac608f
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2025-03-25 14:19:37 +0100

    Fix tests wrt gevent build error

--- tests/scripts/install_odoo.py
+++ tests/scripts/install_odoo.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+import contextlib
 import os
 import subprocess
 import sys
+import tempfile
+from urllib.request import urlopen
 
 odoo_branch = sys.argv[1]
 odoo_dir = sys.argv[2]
@@ -34,6 +37,26 @@ def clone_odoo():
     )
 
 
+@contextlib.contextmanager
+def odoo_requirements(odoo_branch: str):
+    """Yield a list of pip install requirements for Odoo dependencies"""
+    with urlopen(
+        f"https://raw.githubusercontent.com/OCA/OCB/{odoo_branch}/requirements.txt"
+    ) as response:
+        with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
+            reqs = response.read()
+            # Replace gevent and greenlet with versions that have wheels on PyPI.
+            # Remove this when dropping python 3.10 support.
+            reqs = reqs.replace(b"gevent==21.8.0", b"gevent==22.10.2")
+            reqs = reqs.replace(b"greenlet==1.1.2", b"greenlet==2.0.2")
+            tmpfile.write(reqs)
+            tmpfile.close()
+            try:
+                yield ["--no-binary", "psycopg2", "-r", tmpfile.name]
+            finally:
+                os.unlink(tmpfile.name)
+
+
 def install_odoo():
     if odoo_branch in ["11.0", "12.0", "13.0"]:
         # setuptools 58 dropped support for 2to3, which is required
@@ -45,18 +68,8 @@ def install_odoo():
                 "setuptools<58",
             ]
         )
-    subprocess.check_call(
-        [
-            "pip",
-            "install",
-            "--no-binary",
-            "psycopg2",
-            "-r",
-            "https://raw.githubusercontent.com/OCA/OCB/{}/requirements.txt".format(
-                odoo_branch
-            ),
-        ]
-    )
+    with odoo_requirements(odoo_branch) as requirements:
+        subprocess.check_call(["pip", "install", *requirements])
     subprocess.check_call(["pip", "install", "-e", odoo_dir])
 
 
